c0825991e109af3ca41ff308badb6047982c4fde
James Cuénod <j3frea@gmail.com>
2016-11-12 14:20:08 -0600

    Made Collocation Results pure functional component

--- src/components/Modals/CollocationResults.js
+++ src/components/Modals/CollocationResults.js
@@ -1,24 +1,20 @@
 import React from 'react'
 
-class CollocationResults extends React.Component {
-	render() {
-		return (
-			<div>
-				<div className="results_tally">{this.props.data.length} collocations</div>
-				<table className="results_table collocation_table">
-					<tbody>
-						{this.props.data.map(function(row, i){
-							return (
-								<tr key={i}>
-									<td className="hebrew">{row.lexeme}</td>
-									<td className="reference">{row.count}</td>
-								</tr>
-							)
-						}, this)}
-					</tbody>
-				</table>
-			</div>
-		)
-	}
-}
+const CollocationResults = ({data}) => (
+	<div>
+		<div className="results_tally">{data.length} collocations</div>
+		<table className="results_table collocation_table">
+			<tbody>
+				{data.map((row, i) => {
+					return (
+						<tr key={i}>
+							<td className="hebrew">{row.lexeme}</td>
+							<td className="reference">{row.count}</td>
+						</tr>
+					)
+				})}
+			</tbody>
+		</table>
+	</div>
+)
 export default CollocationResults
