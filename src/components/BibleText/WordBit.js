2897e252abf593bbddf10eb0a0d4368525f4df4c
James Cuénod <j3frea@gmail.com>
2016-11-12 10:22:30 -0600

    Convert WordBit to functional es6 const

--- src/components/BibleText/WordBit.js
+++ src/components/BibleText/WordBit.js
@@ -1,23 +1,18 @@
 import React from 'react'
 import EventPropagator from 'events/EventPropagator'
 
-class WordBit extends React.Component {
-	render() {
-		var trailer_to_use = this.props.wordbit.trailer.replace("\n", "  ")
-		return (
-			<span>
-				<span className={this.props.active_wid === this.props.wordbit.wid ? "word_bit active" : "word_bit"}
-						onClick={() => EventPropagator.fireEvent({
-							eventType: "word_clicked",
-							payload: {wid: this.props.wordbit.wid}})
-						}>
-					{this.props.wordbit.bit}
-				</span>
-				<span className="word_trailer">
-					{trailer_to_use}
-				</span>
-			</span>
-		)
-	}
-}
+const WordBit = ({wordbit, active}) => (
+	<span>
+		<span className={active === wordbit.wid ? "word_bit active" : "word_bit"}
+			onClick={() => EventPropagator.fireEvent({
+				eventType: "word_clicked",
+				payload: {wid: wordbit.wid}})
+			}>
+			{wordbit.bit}
+		</span>
+		<span className="word_trailer">
+			{wordbit.trailer.replace("\n", "  ")}
+		</span>
+	</span>
+)
 export default WordBit
