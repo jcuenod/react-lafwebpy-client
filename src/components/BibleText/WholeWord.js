b2bfb9d74f95c78c7330691869946e4c044d3071
James Cuénod <j3frea@gmail.com>
2016-11-23 14:54:08 -0600

    Fixed indentation (doesn't change bundle_

--- src/components/BibleText/WholeWord.js
+++ src/components/BibleText/WholeWord.js
@@ -7,9 +7,9 @@ const WholeWord = ({word_bits, active_wid}) =>(
 	<span className="whole_word">
 		{word_bits.map((bit, i) => {
 			return bit.wid === "verse" ?
-			<span key={i} className="verse_number">{bit.verse}</span> :
+				<span key={i} className="verse_number">{bit.verse}</span> :
 				<WordBit key={i} wordbit={bit} active_wid={active_wid} />
-			})}
-		</span>
+		})}
+	</span>
 )
 export default WholeWord
