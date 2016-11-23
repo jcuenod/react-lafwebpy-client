281b026b7daf4fa88871d83d159940e86c2ebc10
James Cuénod <j3frea@gmail.com>
2016-11-23 14:50:51 -0600

    WholeWord now purely functional

--- src/components/BibleText/WholeWord.js
+++ src/components/BibleText/WholeWord.js
@@ -3,17 +3,13 @@ import {render} from 'react-dom'
 
 import WordBit from './WordBit'
 
-class WholeWord extends React.Component {
-	render() {
-		return (
-			<span className="whole_word">
-				{this.props.word_bits.map((bit, i) => {
-					return bit.wid === "verse" ?
-						<span key={i} className="verse_number">{bit.verse}</span> :
-						<WordBit key={i} wordbit={bit} active_wid={this.props.active_wid} />
-				}, this)}
-			</span>
-		)
-	}
-}
+const WholeWord = ({word_bits, active_wid}) =>(
+	<span className="whole_word">
+		{word_bits.map((bit, i) => {
+			return bit.wid === "verse" ?
+			<span key={i} className="verse_number">{bit.verse}</span> :
+				<WordBit key={i} wordbit={bit} active_wid={active_wid} />
+			})}
+		</span>
+)
 export default WholeWord
