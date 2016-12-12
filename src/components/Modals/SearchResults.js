fe3ae6a833fe9310764cc7f9bfbef798daf57d5f
James Cuénod <j3frea@gmail.com>
2016-12-12 17:38:10 -0600

    Remove spaces from urls in search results so '1 Kings' works

--- src/components/Modals/SearchResults.js
+++ src/components/Modals/SearchResults.js
@@ -7,7 +7,7 @@ const SearchResults = ({data}) => (
 		message={<span>Scripture quoted by permission. All scripture quotations, unless otherwise indicated, are taken from the <a href='http://netbible.org'>NET Bible</a>® copyright ©1996-2016 by Biblical Studies Press, L.L.C. All rights reserved.</span>}>
 		{data.map((row, i) => (
 			<tr key={i}>
-				<td className="reference"><a href={row.passage.replace(/(.*)\ (\d+):\d+/, "/$1/$2")}>{row.passage}</a></td>
+				<td className="reference"><a href={row.passage.replace(/(.*)\ (\d+):\d+/, "/$1/$2").replace(" ", "")}>{row.passage}</a></td>
 				<td className="hebrew">{row.clause.map((words, i) => (
 						<span key={i} className={words.significance}>{words.text}</span>
 					))}</td>
