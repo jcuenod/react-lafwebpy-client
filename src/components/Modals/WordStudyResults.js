9dedd16e40211e66d7fbf379d764ff4632926797
James Cuénod <j3frea@gmail.com>
2017-02-06 12:50:08 -0600

    Improved display of columns in Word Study

--- src/components/Modals/WordStudyResults.js
+++ src/components/Modals/WordStudyResults.js
@@ -12,13 +12,47 @@ class WordStudyResults extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
+			visibleColumns: [
+				{
+					"value": "lex",
+					"label": "lex"
+				},{
+					"value": "tricons",
+					"label": "tricons"
+				},{
+					"value": "sdbh",
+					"label": "sdbh"
+				},{
+					"value": "lxxlexeme",
+					"label": "lxxlexeme"
+				},{
+					"value": "vs",
+					"label": "vs"
+				},{
+					"value": "vt",
+					"label": "vt"
+				},{
+					"value": "book",
+					"label": "book"
+				},{
+					"value": "ch",
+					"label": "ch"
+				},{
+					"value": "v",
+					"label": "v"
+				},
+			],
 			pivotColumns: [],
 		}
 	}
 	render() {
 		var cols = this.props.data.search_results.columns
 		var select_options = cols.map((x) => ({ "value": x["accessor"], "label": x["header"]}))
-		var cols = cols.map((x) => {x["minWidth"] = undefined; return x})
+		var cols = cols.map((x) => {
+			x["minWidth"] = undefined
+			x["show"] = this.state.visibleColumns.map((y) => y["value"]).includes(x["accessor"])
+			return x
+		})
 		var rows = this.props.data.search_results.rows
 		// var rows = this.props.data.search_results.rows.map((r) => {
 		// 	keys = Object.keys(r)
@@ -28,13 +62,26 @@ class WordStudyResults extends React.Component {
 		return (
 			<div>
 				<h1>Word Study Results ({rows.length})</h1>
-				<Select
-					name="pivots"
-					value={this.state.pivotColumns}
-					options={select_options}
-					multi={true}
-					onChange={(v) => this.setState({ "pivotColumns": v })}
-					/>
+				<div style={{width: "48%", display: "inline-block", margin: "0 1%"}}>
+					Visible Columns:
+					<Select
+						name="visibles"
+						value={this.state.visibleColumns}
+						options={select_options}
+						multi={true}
+						onChange={(v) => this.setState({ "visibleColumns": v })}
+						/>
+				</div>
+				<div style={{width: "48%", display: "inline-block", margin: "0 1%"}}>
+					Pivoting Columns:
+					<Select
+						name="pivots"
+						value={this.state.pivotColumns}
+						options={select_options}
+						multi={true}
+						onChange={(v) => this.setState({ "pivotColumns": v })}
+						/>
+				</div>
 				<br />
 				<ReactTable
 					data={rows}
