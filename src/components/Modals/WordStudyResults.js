f0a38bdb3137c056706e220907716b138587c497
James Cuénod <j3frea@gmail.com>
2017-03-25 23:34:59 -0500

    Fix column ordering in word study results

--- src/components/Modals/WordStudyResults.js
+++ src/components/Modals/WordStudyResults.js
@@ -11,28 +11,29 @@ import term_to_english from 'data/MorphCodes'
 class WordStudyResults extends React.Component {
 	constructor(props) {
 		super(props)
+		var possibleVisible = this.props.data.search_results.columns.map(x => x["accessor"])
+		var visibleDefault = [
+			{ "value": "lex", "label": "lex"},
+			{ "value": "tricons", "label": "tricons"},
+			{ "value": "sdbh", "label": "sdbh"},
+			{ "value": "lxxlexeme", "label": "lxxlexeme"},
+			{ "value": "vs", "label": "vs"},
+			{ "value": "vt", "label": "vt"},
+			{ "value": "book", "label": "book"},
+			{ "value": "ch", "label": "ch"},
+			{ "value": "v", "label": "v"},
+		].filter(x => possibleVisible.includes(x["value"]))
 		this.state = {
-			visibleColumns: [
-				{ "value": "lex", "label": "lex"},
-				{ "value": "tricons", "label": "tricons"},
-				{ "value": "sdbh", "label": "sdbh"},
-				{ "value": "lxxlexeme", "label": "lxxlexeme"},
-				{ "value": "vs", "label": "vs"},
-				{ "value": "vt", "label": "vt"},
-				{ "value": "book", "label": "book"},
-				{ "value": "ch", "label": "ch"},
-				{ "value": "v", "label": "v"},
-			],
+			visibleColumns: visibleDefault,
 			pivotColumns: [],
 		}
 	}
 	render() {
-		var cols = this.props.data.search_results.columns
-		var select_options = cols.map((x) => ({ "value": x["accessor"], "label": x["header"]}))
-		var cols = cols.map((x) => {
-			x["minWidth"] = undefined
-			x["show"] = this.state.visibleColumns.map((y) => y["value"]).includes(x["accessor"])
-			return x
+		var select_options = this.props.data.search_results.columns.map((x) => ({ "value": x["accessor"], "label": x["header"]}))
+		var cols = this.state.visibleColumns.map((c) => {
+			var column_data = this.props.data.search_results.columns.find((x) => x["accessor"] === c["value"])
+			column_data["minWidth"] = undefined
+			return column_data
 		})
 		var rows = this.props.data.search_results.rows
 		return (
